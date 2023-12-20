b927506f82240ca920bfd4167209684269166b03
ghanahmada <ghanamada@gmail.com>
2023-12-20 23:57:46 +0700

    finish

--- lib/apps/profile/models/wishlistbook.dart
+++ lib/apps/profile/models/wishlistbook.dart
@@ -1,29 +1,29 @@
-// wishlist_book.dart
+// // wishlist_book.dart
 
-class WishlistBook {
-  final int id;
-  final String title;
-  final String imageLink;
+// class WishlistBook {
+//   final int id;
+//   final String title;
+//   final String imageLink;
 
-  WishlistBook({
-    required this.id,
-    required this.title,
-    required this.imageLink,
-  });
+//   WishlistBook({
+//     required this.id,
+//     required this.title,
+//     required this.imageLink,
+//   });
 
-  factory WishlistBook.fromJson(Map<String, dynamic> json) {
-    return WishlistBook(
-      id: json['id'],
-      title: json['title'],
-      imageLink: json['image_link'],
-    );
-  }
+//   factory WishlistBook.fromJson(Map<String, dynamic> json) {
+//     return WishlistBook(
+//       id: json['id'],
+//       title: json['title'],
+//       imageLink: json['image_link'],
+//     );
+//   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'image_link': imageLink,
-    };
-  }
-}
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'id': id,
+//       'title': title,
+//       'image_link': imageLink,
+//     };
+//   }
+// }
